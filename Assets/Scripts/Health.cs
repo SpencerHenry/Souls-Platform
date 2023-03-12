1be9431b69e6eb6c0fd1526dc22996705e40b5ae
spencer.henry182@myci.csuci.edu <spencer.henry182@myci.csuci.edu>
2023-03-11 16:44:28 -0800

    Revert "restore"
    This reverts commit 6b9c019a3f4f4af2e6408913b4b02cb94db4a81e.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,19 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public static float totalHealth = 100;
-    private RectTransform healthBar;
-    
-    public void updateHealthBar(float size)
-    {
-        healthBar.localScale = new Vector3(size, 1f);
-    }
-
-      
-
-
+    public static float totalHealth = 1f;
+ 
 }
