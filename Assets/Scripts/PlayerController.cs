755e20217a4937fdd4c57f6cb99d9d0e1ab2683c
LuisAvelar <luis_avelar17@gmail.com>
2023-03-12 17:56:29 -0700

    finished idle, run animation. connected levels together

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,19 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb2D;
-
+    private Animator animate;
     private float moveSpeed;
     private float jumpForce;
     private bool isJumping;
     private float moveHorizontal;
     private float moveVertical;
+    private bool facingRight = true;
     // Start is called before the first frame update
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-        
+        animate = gameObject.GetComponent<Animator>();
+
         moveSpeed = 3f;
         jumpForce = 60f;
         isJumping = false;
@@ -28,13 +30,22 @@ public class PlayerController : MonoBehaviour
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
+        animate.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+
+        if(moveHorizontal >  0 && !facingRight) {
+            flip();
+        } else if(moveHorizontal <  0 && facingRight) {
+            flip();
+        }
+
     }
 
     void FixedUpdate() {
 
         if(moveHorizontal > 0.1f || moveHorizontal < -0.1f) {
             rb2D.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
-        
+            
+
         }
 
         if(!isJumping && moveVertical > 0.1f) {
@@ -53,6 +64,13 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.tag == "Platform") {
             isJumping = true;
         }
+    }
+
+    void flip() {
+        facingRight = !facingRight;
+        Vector2 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
     }
 
 }
