ce499334d1b143d5bf170ef6f56fdb14ec610718
KaiMurray <rgbkai@gmail.com>
2023-04-05 17:54:48 -0700

    Undo merge from develop to feature that overwrote feature
    This reverts commit f1b58e8e46a3080d948e04977b3df32f4f1c5858, reversing
    changes made to 7f2c4ae4a56e4fd32a50be33f03e812bb1789860.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu...");
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -10,6 +10,16 @@ public class LevelLoader : MonoBehaviour
 
     public float transitionTime = 1f;
 
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    LoadNextLevel();
+        //}   
+    }
+
+
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
