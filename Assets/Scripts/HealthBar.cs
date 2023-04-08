558e832fa90bfa4be43c5ad0da4712439e336ef9
spencer.henry182@myci.csuci.edu <spencer.henry182@myci.csuci.edu>
2023-04-08 14:48:41 -0700

    Ui updates
    UI visual updates
    remove text
    comment out respawn (unreference)

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,27 +7,17 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Text healthText;
     private RectTransform bar;
 
     private void Start()
     {
         bar = GetComponent<RectTransform>();
         SetSize(Health.totalHealth); // To Retain Health when switching between scenes
-        healthText.text = "HP:\t\t\t\t" + Health.totalHealth;
     }
 
     private void Update()
     {
         SetSize(Health.totalHealth);
-        if(Health.totalHealth > 0)
-        {
-            healthText.text = "HP:\t\t\t\t" + Health.totalHealth;
-        }
-        else
-        {
-            healthText.text = "\t\tDEAD";
-        }
     }
 
     public void SetSize(float size)
