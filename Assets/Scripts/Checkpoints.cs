3f91d9a161bf0298f815878b8ebca58dd1ca8778
Davis Umann <umann17@hotmail.com>
2023-04-13 10:58:19 -0700

    Created prefab for win, tested and complete

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Checkpoints : MonoBehaviour
 {
    public static Vector3 currCheckPoint;
    public static bool checkpointSet = false;
+   public Text WINTEXT;
    private void Start()
    {
        if(checkpointSet)
@@ -22,6 +24,10 @@ public class Checkpoints : MonoBehaviour
 
    private void OnTriggerEnter2D(Collider2D col)
    {
+        if(col.tag == "Win")
+        {
+            WINTEXT.gameObject.SetActive(true);
+        }
         if(col.tag == "CheckPoint")
         {
             currCheckPoint = transform.position;
