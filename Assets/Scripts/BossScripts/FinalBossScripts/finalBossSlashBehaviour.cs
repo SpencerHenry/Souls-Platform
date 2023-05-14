1f185e00fcdd6a79f7269e6b7508122923f592cd
LuisAvelar <luis_avelar17@gmail.com>
2023-05-13 21:02:11 -0700

    boss almost done

--- Assets/Scripts/BossScripts/FinalBossScripts/finalBossSlashBehaviour.cs
+++ Assets/Scripts/BossScripts/FinalBossScripts/finalBossSlashBehaviour.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class finalBossSlashBehaviour : StateMachineBehaviour
 {
+     public float timer;
+    public float minTime;
+    public float maxTime;
+
+    private Vector2 playerPos;
+    public float speed;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+        timer = Random.Range(minTime, maxTime);
+        //animator.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 3f, ForceMode2D.Impulse);
+
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (timer <= 0) 
+        {
+            animator.SetTrigger("idle");
+        } else {
+            timer -= Time.deltaTime;
+        }
+
+        Vector2 target = new Vector2(playerPos.x, animator.transform.position.y);
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
+        
+
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
 }
